ccbe671716d36215f4b8adb4a8900a6dfd0b1f21
Marko <myemailwhichdefinitelyexists@asfdsgdlkdmasdfklnkljadsnflkuh.com>
2025-06-10 09:40:36 +0200

    Basic Loop In Pygame

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+running = True
+
+while running:
+	for event in pygame.event.get(): #get all events
+		if event.type == pygame.QUIT: #stop running if event is quit
+			running = False
+
+	screen.fill("blue") # set bg to blue
+	pygame.display.flip() #render
+
+pygame.quit()
